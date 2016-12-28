5c40c4e7bc9b1426c15ec0237628b3ee3ec1337c
Michael Caley <mjcaley@darkarctic.com>
2016-12-27 22:18:37 -0500

    Converted to use asyncio Added basic test script

--- spamc/client.py
+++ spamc/client.py
@@ -1,128 +1,94 @@
 #!/usr/bin/env python3
 
-import re
-import socket
+import asyncio
 
 from spamc.requests import *
 from spamc.responses import *
 
-
-SERVER_SIGNATURE = re.compile(r'^SPAMD/\d+\.\d+ ') 
-
-class BadResponse(Exception):
-    def __init__(self, response):
-        self.response = response
         
-class NoSSL(Exception):
-    def __init__(self, description):
-        self.description = description
-        
-class Client:
-    def __init__(self, host = 'localhost', port = 783, ip_version = 4, user = None, compress = False, ssl = False, **ssl_options):
+class Client():
+    def __init__(self, host = 'localhost', port = 783, user = None, compress = False, ssl = False, loop=None):
         self.host = host
         self.port = port
-        self.ip_version = ip_version
         self.user = user
         self.compress = compress
         self.ssl = ssl
-        self.ssl_options = ssl_options
+        self.loop = loop
         
-    def connect(self):
-        if self.ip_version is 4:
-            sock = socket.socket(socket.AF_INET)
-        elif self.ip_version is 6:
-            sock = socket.socket(socket.AF_INET6)
-        else:
-            raise OSError('Invalid address family, must be IPv 4 or 6')
-        sock.connect((self.host, self.port))
+    async def connect(self):
+        reader, writer = await asyncio.open_connection(self.host, self.port, loop=self.loop, ssl=self.ssl)
+        return reader, writer
         
-        if self.ssl:
-            try:
-                import ssl
-            except ImportError:
-                raise NoSSL('SSL is not supported by this installation')
-                
-            ssl_sock = ssl.wrap_socket(sock, **self.ssl_options)
-            return ssl_sock
-        else:
-            return sock
-        
-    def send(self, request):
-        conn = self.connect()
-        conn.send(bytes(request))
-        data = bytes()
-        while True:
-            received = conn.recv(1024)
-            if not received:
-                break
-            data += received
+    async def send(self, request):
+        reader, writer = await self.connect()
+        writer.write(bytes(request))
+        data = await reader.read()
         response = SpamdResponse.parse(data.decode())
-        conn.close()
         
         return response
     
-    def check(self, message):
+    async def check(self, message):
         request = Check(message = message, compress = self.compress)
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
         
-    def headers(self, message):
+    async def headers(self, message):
         request = Headers(message = message, compress = self.compress)
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
         
-    def ping(self):
+    async def ping(self):
         request = Ping()
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
     
-    def process(self, message):
+    async def process(self, message):
         request = Process(message = message, compress = self.compress)
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
     
-    def report(self, message):
+    async def report(self, message):
         request = Report(message = message, compress = self.compress)
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
     
-    def report_if_spam(self, message):
+    async def report_if_spam(self, message):
         request = ReportIfSpam(message = message, compress = self.compress)
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
     
-    def symbols(self, message):
+    async def symbols(self, message):
         request = Symbols(message = message, compress = self.compress)
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
     
-    def tell(self,
-             message_class: MessageClassOption,
-             message,
-             set_destinations = {'local': False, 'remote': False},
-             remove_destinations = {'local': False, 'remote': False}
-            ):
+    async def tell(self,
+                   message_class: MessageClassOption,
+                   message,
+                   set_destinations = {'local': False, 'remote': False},
+                   remove_destinations = {'local': False, 'remote': False}
+                ):
         request = Tell(message,
                        [ MessageClass(message_class),
                          Set(**set_destinations),
@@ -131,7 +97,6 @@ class Client:
                        compress = self.compress)
         if self.user:
             request.headers.append(User(self.user))
-        response = self.send(request)
+        response = await self.send(request)
         
         return response
-        
---
+++ test/test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+import pathlib
+import sys
+
+path = pathlib.Path('..')
+sys.path.append(str(path.resolve()))
+
+import spamc
+
+        
+def test():
+    import email
+    gtube_msg = email.message_from_string('''Subject: Test spam mail (GTUBE)
+Message-ID: <GTUBE1.1010101@example.net>
+Date: Wed, 23 Jul 2003 23:30:00 +0200
+From: Sender <sender@example.net>
+To: Recipient <recipient@example.net>
+Precedence: junk
+MIME-Version: 1.0
+Content-Type: text/plain; charset=us-ascii
+Content-Transfer-Encoding: 7bit
+
+This is the GTUBE, the
+	Generic
+	Test for
+	Unsolicited
+	Bulk
+	Email
+
+If your spam filter supports it, the GTUBE provides a test by which you
+can verify that the filter is installed correctly and is detecting incoming
+spam. You can send yourself a test mail containing the following string of
+characters (in upper case and with no white spaces and line breaks):
+
+XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X
+
+You should send this test mail from an account outside of your network.
+''')
+    
+    async def print_response(title, func, *opts):
+        try:
+            resp = await func(*opts)
+            print(title, repr(resp))
+        except Exception as e:
+            logging.error(' '.join(['Error:', str(e)]))
+    
+    loop = asyncio.new_event_loop()
+    client = spamc.Client(loop=loop)
+    
+    loop.run_until_complete(
+        asyncio.gather(
+            print_response('Ping 1', client.ping),
+            print_response('Check 1', client.check, gtube_msg),
+            print_response('Headers 1', client.headers, gtube_msg),
+            print_response('Process 1', client.process, gtube_msg),
+            print_response('Symbols 1', client.symbols, gtube_msg),
+            print_response('Ping 2', client.ping),
+            loop=loop)
+    )
+    
+    loop.close()
+        
+if __name__ == '__main__':
+    test()
