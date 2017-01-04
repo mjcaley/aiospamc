a24c165b44bc606c3a93ecd7db8566be774e9320
Michael Caley <mjcaley@darkarctic.com>
2017-01-03 21:31:40 -0500

    Added docstrings to the requests module

--- aiospamc/requests.py
+++ aiospamc/requests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+'''Contains all requests that can be made to the SPAMD service.'''
+
 from aiospamc.content_man import BodyHeaderManager
 from aiospamc.transport import Outbound
 
@@ -11,7 +13,20 @@ class SPAMCRequest(BodyHeaderManager, Outbound):
     request = b'%(verb)b %(protocol)b\r\n%(headers)b\r\n%(body)b'
 
     def __init__(self, verb, body=None, *headers):
-        ''''''
+        '''SPAMCRequest constructor.
+
+        Parameters
+        ----------
+        verb : str
+            Method name of the request.
+        body : str
+            String representation of the body.  An instance of the
+            aiospamc.headers.ContentLength will be automatically added.
+        *headers : tuple of aiospamc.headers.Header
+            Collection of headers to be added.  If it contains an instance of
+            aiospamc.headers.Compress then the body is automatically
+            compressed.
+        '''
 
         self.verb = verb.encode()
         super().__init__(body, *headers)
@@ -20,11 +35,12 @@ class SPAMCRequest(BodyHeaderManager, Outbound):
         return self.compose()
 
     def __repr__(self):
-        return '{}({}, {}, {})'.format(self.__class__.__name__, self.verb, self._headers, self.body)
+        return '{}({}, {}, {})'.format(self.__class__.__name__,
+                                       self.verb,
+                                       self._headers,
+                                       self.body)
 
     def compose(self):
-        '''Composes a request based on the verb and headers that are currently set.'''
-
         request = self.request % {b'verb': self.verb,
                                   b'protocol': self.protocol,
                                   b'headers': b''.join(map(bytes, self._headers.values())),
@@ -33,6 +49,10 @@ class SPAMCRequest(BodyHeaderManager, Outbound):
         return request
 
 class Check(SPAMCRequest):
+    '''CHECK request.  Send a message to be checked whether it's spam or not.
+    The response will contain a Spam header.
+    '''
+
     def __init__(self, message, *headers):
         super().__init__('CHECK', message, *headers)
 
@@ -42,6 +62,11 @@ class Check(SPAMCRequest):
                                                    self._headers)
 
 class Headers(SPAMCRequest):
+    '''HEADERS request.  Send a message to be checked whether it's spam or not.
+    Response will contain a Spam header and the body will contain the modified
+    email headers.
+    '''
+
     def __init__(self, message, *headers):
         super().__init__('HEADERS', message, *headers)
 
@@ -51,6 +76,8 @@ class Headers(SPAMCRequest):
                                                    self._headers)
 
 class Ping(SPAMCRequest):
+    '''PING request.  Send a request to see if the SPAMD service is alive.'''
+
     def __init__(self, *headers):
         super().__init__('PING', body=None, *headers)
 
@@ -58,6 +85,11 @@ class Ping(SPAMCRequest):
         return '{}(headers={})'.format(self.__class__.__name__, self._headers)
 
 class Process(SPAMCRequest):
+    '''PROCESS request.  Send a message to be checked whether it's spam or not.
+    Response will contain a Spam header and the body will contain the modified
+    message.
+    '''
+
     def __init__(self, message, *headers):
         super().__init__('PROCESS', message, *headers)
 
@@ -67,6 +99,10 @@ class Process(SPAMCRequest):
                                                    self._headers)
 
 class Report(SPAMCRequest):
+    '''REPORT request.  Send a message to be checked whether it's spam or not.
+    Response will contain a Spam header and the body will contain a report.
+    '''
+
     def __init__(self, message, *headers):
         super().__init__('REPORT', message, *headers)
 
@@ -76,6 +112,11 @@ class Report(SPAMCRequest):
                                                    self._headers)
 
 class ReportIfSpam(SPAMCRequest):
+    '''REPORT_IFSPAM request.  Send a message to be checked whether it's spam
+    or not.  Response will contain a Spam header and the body will contain a
+    report if it is spam.
+    '''
+
     def __init__(self, message, *headers):
         super().__init__('REPORT_IFSPAM', message, *headers)
 
@@ -85,6 +126,11 @@ class ReportIfSpam(SPAMCRequest):
                                                    self._headers)
 
 class Symbols(SPAMCRequest):
+    '''SYMBOLS request.  Send a message to be checked whether it's spam or not.
+    Response will contain a Spam header and the body will contain a comma
+    separated list of SpamAssassin rules that were hit.
+    '''
+
     def __init__(self, message, *headers):
         super().__init__('SYMBOLS', message, *headers)
 
@@ -94,6 +140,11 @@ class Symbols(SPAMCRequest):
                                                    self._headers)
 
 class Tell(SPAMCRequest):
+    '''TELL request.  Instruct the SPAMD service to perform an action on a
+    message (learning, reporting, forgetting, and/or revoking).  The SPAMD
+    service must have --allow-tell.
+    '''
+
     def __init__(self, message, *headers):
         super().__init__('TELL', message, *headers)
 
