72fba3672823a3e302a87cf10b48b5c6dd512927
Michael Caley <mjcaley@darkarctic.com>
2019-09-05 09:17:46 -0400

    Updated minimum version required for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '2.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aiospamc'
-copyright = '2016-2018, Michael Caley'
+copyright = '2016-2019, Michael Caley'
 author = 'Michael Caley'
 
 # The version info for the project you're documenting, acts as replacement for
