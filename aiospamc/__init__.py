165aea3b43065f2ffd96f4a2592d1b7d71671175
Michael Caley <mjcaley@darkarctic.com>
2019-10-12 16:16:06 -0400

    Increment version to 0.6.0-pre1

--- aiospamc/__init__.py
+++ aiospamc/__init__.py
@@ -14,5 +14,5 @@ __all__ = ('Client',
 __author__ = 'Michael Caley'
 __copyright__ = 'Copyright 2016-2019 Michael Caley'
 __license__ = 'MIT'
-__version__ = '0.5.0'
+__version__ = '0.6.0-pre1'
 __email__ = 'mjcaley@darkarctic.com'
