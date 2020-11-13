ff46555fb8bf032b93b2920cf944e55beeb3001e
Michael Caley <mjcaley@darkarctic.com>
2020-11-12 23:24:36 -0500

    Added tests to ensure frontend functions are imported to the module

---
+++ tests/test_init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import aiospamc
+
+import pytest
+
+
+@pytest.mark.parametrize("func_name", [
+    "check", "headers", "ping", "process", "report", "report_if_spam", "symbols", "tell"
+])
+def test_functions(func_name):
+    assert hasattr(aiospamc, func_name)
