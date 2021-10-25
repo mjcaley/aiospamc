fb13e7d528378dcc7e6c5256db97c23f18825712
Michael Caley <mjcaley@darkarctic.com>
2021-10-24 21:09:18 -0400

    Added return type hints for repr and str methods

--- aiospamc/requests.py
+++ aiospamc/requests.py
@@ -59,10 +59,10 @@ class Request:
             b"body": body,
         }
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (
             f"<{self.__class__.__module__}.{self.__class__.__qualname__} "
             f"[{self.verb}] "
