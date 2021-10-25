e23c8afa48efe616fe08df709a7f3c328245b941
Michael Caley <mjcaley@darkarctic.com>
2021-10-24 21:31:37 -0400

    Fixed bug #317 Limited the number of splits for the status line

--- aiospamc/incremental_parser.py
+++ aiospamc/incremental_parser.py
@@ -283,7 +283,7 @@ def parse_response_status(stream: bytes) -> Dict[str, Union[str, int]]:
 
     try:
         protocol_version, status_code, message = list(
-            filter(None, stream.decode("ascii").split(" "))
+            filter(None, stream.decode("ascii").split(" ", 2))
         )
         protocol, version = protocol_version.split("/")
     except ValueError as error:
