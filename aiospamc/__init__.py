b0899ce0e33e999cf9c3d9ace9f64cecc9f2270b
Michael Caley <mjcaley@darkarctic.com>
2024-10-09 12:13:31 -0400

    Bump to 1.1.1

--- aiospamc/__init__.py
+++ aiospamc/__init__.py
@@ -20,7 +20,7 @@ from .header_values import ActionOption, MessageClassOption
 __author__ = "Michael Caley"
 __copyright__ = "Copyright 2016-2023 Michael Caley"
 __license__ = "MIT"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __email__ = "mjcaley@darkarctic.com"
 
 logger.disable(__package__)
